ff22c33e5f0c570466766ae3dc10a2de61a03c29
Soumyajit Saha <57801484+Soumyajit10@users.noreply.github.com>
2019-11-17 15:27:34 +0530

    Update HS_all_feat.m

--- HS_all_feat.m
+++ HS_all_feat.m
@@ -23,7 +23,7 @@ while(it<10)
                 p1=rand;
                 if(p1<HMCR)
                     while(1) % selecting a feature from any subset in the Harmony memory
-                        row_in_subset=randi([1,10]);
+                        row_in_subset=randi([1,HMS]);
                         col_in_subset=randi([1,col-1]);
                         if(HM(row_in_subset,col_in_subset)==1)
                             break;
