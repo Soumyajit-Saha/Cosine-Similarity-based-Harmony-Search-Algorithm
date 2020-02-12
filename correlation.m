807577d4514c1bebfa840c856fc3197a35f38b91
Soumyajit Saha <57801484+Soumyajit10@users.noreply.github.com>
2020-02-12 21:42:59 +0530

    Update correlation.m

--- correlation.m
+++ correlation.m
@@ -1,4 +1,4 @@
-function [ diff_pearson,diff_spearman,diff_kendall ] = correlation( str )
+function [ diff_pearson ] = correlation( str )
 x=textread(str,'%s','bufsize',50000);
 x=split(x,',');
 [row,col]=size(x);
@@ -21,111 +21,55 @@ for i=1:col-1
    % t=t';
    % out=py.pc.pc(t,y);
    [out1,~]=corr(t,y);
-   [out2,~]=corr(t,y,'Type','Spearman');
-   [out3,~]=corr(t,y,'Type','Kendall');
+   %[out2,~]=corr(t,y,'Type','Spearman');
+   %[out3,~]=corr(t,y,'Type','Kendall');
     if(isnan(out1)==true)           
            out1=0;
     end
-    if(isnan(out2)==true)           
-           out2=0;
-    end
-    if(isnan(out3)==true)           
-           out3=0;
-    end
+    %if(isnan(out2)==true)           
+     %      out2=0;
+    %end
+    %if(isnan(out3)==true)           
+    %       out3=0;
+    %end
     d1=d1+out1;
-    d2=d2+out2;
-    d3=d3+out3;
+    %d2=d2+out2;
+    %d3=d3+out3;
 end
 d1=d1/(col-1);
-d2=d2/(col-1);
-d3=d3/(col-1);
+%d2=d2/(col-1);
+%d3=d3/(col-1);
 r1=0;
-r2=0;
-r3=0;
+%r2=0;
+%r3=0;
 for i=1:col-1
     for j=1:col-1
         tx=x(:,i);
         ty=x(:,j);
         %res=py.pc.pc(tx,ty);
         [res1,~]=corr(tx,ty);
-        [res2,~]=corr(tx,ty,'Type','Spearman');
-        [res3,~]=corr(tx,ty,'Type','Kendall');
+        %[res2,~]=corr(tx,ty,'Type','Spearman');
+        %[res3,~]=corr(tx,ty,'Type','Kendall');
         if(isnan(res1)==true)           
             res1=0;
         end
-        if(isnan(res2)==true)           
-            res2=0;
-        end
-        if(isnan(res3)==true)           
-            res3=0;
-        end
+        %if(isnan(res2)==true)           
+         %   res2=0;
+        %end
+        %if(isnan(res3)==true)           
+         %   res3=0;
+        %end
         r1=r1+res1;
-        r2=r2+res2;
-        r3=r3+res3;
+        %r2=r2+res2;
+        %r3=r3+res3;
     end
 end
 r1=r1/(col-1)^2;
-r2=r2/(col-1)^2;
-r3=r3/(col-1)^2;
+%r2=r2/(col-1)^2;
+%r3=r3/(col-1)^2;
 diff_pearson=d1-r1;
-diff_spearman=d2-r2;
-diff_kendall=d3-r3;
+%diff_spearman=d2-r2;
+%diff_kendall=d3-r3;
 
 
-% d=0;
-% for i=1:col-1
-%     t=x(:,i);
-%    % t=t';
-%    % out=py.pc.pc(t,y);
-%    [out,~]=corr(t,y,'Type','Spearman');
-%     if(isnan(out)==true)           
-%            out=0;
-%     end
-%     d=d+out;
-% end
-% d=d/(col-1);
-% r=0;
-% for i=1:col-1
-%     for j=1:col-1
-%         tx=x(:,i);
-%         ty=x(:,j);
-%         %res=py.pc.pc(tx,ty);
-%         [res,~]=corr(tx,ty,'Type','Spearman');
-%         if(isnan(res)==true)           
-%             res=0;
-%         end
-%         r=r+res;
-%     end
-% end
-% r=r/(col-1)^2;
-% diff_spearman=d-r;
-% 
-% d=0;
-% for i=1:col-1
-%     t=x(:,i);
-%    % t=t';
-%    % out=py.pc.pc(t,y);
-%    [out,~]=corr(t,y,'Type','Kendall');
-%     if(isnan(out)==true)           
-%            out=0;
-%     end
-%     d=d+out;
-% end
-% d=d/(col-1);
-% r=0;
-% for i=1:col-1
-%     for j=1:col-1
-%         tx=x(:,i);
-%         ty=x(:,j);
-%         %res=py.pc.pc(tx,ty);
-%         [res,~]=corr(tx,ty,'Type','Kendall');
-%         if(isnan(res)==true)           
-%             res=0;
-%         end
-%         r=r+res;
-%     end
-% end
-% r=r/(col-1)^2;
-% diff_kendall=d-r;
-% end
-% 
+end
