5e7f0778e49a966600ee5675682299e65bd8bf15
Soumyajit Saha <57801484+Soumyajit-Saha@users.noreply.github.com>
2020-10-06 01:15:41 +0530

    Update normal.m

--- normal.m
+++ normal.m
@@ -6,19 +6,8 @@ m1=m1';
 p=p(2:row,1:col-1);
 p=str2double(p);
 
-
+count=1;
 ch=m1(:,1);
-     count=1;
-     count_in_class=0;
-     for i=1:row-1
-         if(strcmp(m1(1,i),ch)==1)
-             count_in_class=count_in_class+1;
-             ch=m1(1,i);
-         else
-             break;
-         end
-     end
-     ch=m1(:,1);
      for i=1:row-1
          if(strcmp(m1(1,i),ch)~=1)
              count=count+1;
@@ -26,60 +15,25 @@ ch=m1(:,1);
          end
          y(i)=count;
      end
-     %m1=m1';
- %     p=p(2:row,1:col-1);
- %     p=str2double(p);
-     train_ratio=0.66;
-     test_ratio=0.34;
-     train_arr=zeros(1,row-1);
-     X_train=zeros(int32(train_ratio*count_in_class)*max(y),col-1);
-     Y_train=zeros(int32(train_ratio*count_in_class)*max(y),1);
-     X_test=zeros(int32((row-1)-int32(train_ratio*count_in_class)*max(y)),col-1);
-     Y_test=zeros(int32((row-1)-int32(train_ratio*count_in_class)*max(y)),1);
-     in1=1;
-     in2=1;
-     for cl=1:max(y)
-         i=1;
-         while(i<int32(train_ratio*(count_in_class))+1)
-             inst=randi([(cl-1)*count_in_class+1,cl*count_in_class]);
-             if(train_arr(1,inst)==1)
-                 continue;
-             else
-                
-             X_train(in1,:)=p(inst,:);
-             Y_train(in1,:)=y(inst);
-             train_arr(1,inst)=1;
-             in1
-             in1=in1+1;
-             
-             end
-          i=i+1;   
-         end
-         i=1;
-         while(i<int32(count_in_class-train_ratio*(count_in_class))+1)
-             inst=randi([(cl-1)*count_in_class+1,cl*count_in_class]);
-             if(train_arr(1,inst)==1)
-                 continue;
-             else
-                 
-             X_test(in2,:)=p(inst,:);
-             Y_test(in2,:)=y(inst);
-             train_arr(1,inst)=1;
-             in2
-             in2=in2+1;
-             
-            end
-             i=i+1;
-         end
-    end
- m=X_train;
- m1=Y_train
+     
+rng('default');
+% Cross varidation (train: 70%, test: 30%)
+cv = cvpartition(size(p,1),'HoldOut',0.34);
+idx = cv.test;
+% Separate to training and test data
+X_train = p(~idx,:);
+Y_train = y(~idx);
+X_test  = p(idx,:);
+Y_test = y(idx);
+
+m=X_train;
+m1=Y_train;
  c=1;
  for k=1:col-1
      max_value=max(X_train(:,k));
      min_value=min(X_train(:,k));
      if((max_value-min_value)~=0)
-         m(:,c)=p(:,k);
+         m(:,c)=X_train(:,k);
          c=c+1;
      end
  end
@@ -98,4 +52,3 @@ for i=1:col-1
         sim_mat(i,j)=CosineSimilarity(p(:,i),p(:,j));
     end
 end
-          
